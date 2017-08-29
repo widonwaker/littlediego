a2fdd0e5a7aeeb586394870661b53f92cf67f955
widonwaker <hey.lol@live.it>
2017-08-29 18:13:31 +0200

    Add files via upload

---
+++ js/eng.js
@@ -0,0 +1,5 @@
+document.getElementById("tabB").innerHTML = 'Profiles';
+document.getElementById("contactstab").innerHTML = 'Add New';
+document.getElementById("opzioni").innerHTML = 'Options';
+document.getElementById("unitmisura").innerHTML = 'Unit of measure';
+document.getElementById("saveopt").value = 'Save';
---
+++ js/ita.js
@@ -0,0 +1,5 @@
+document.getElementById("tabB").innerHTML = 'Profili';
+document.getElementById("contactstab").innerHTML = 'Nuova Misura';
+document.getElementById("opzioni").innerHTML = 'Opzioni';
+document.getElementById("unitmisura").innerHTML = 'Unità di misura:';
+document.getElementById("saveopt").value = 'Salva';
