2c3a3f1f2fcc9783e3a9a2bf3275577eef752268
widonwaker <hey.lol@live.it>
2017-08-29 19:52:24 +0200

    Add files via upload

--- js/eng.js
+++ js/eng.js
@@ -3,3 +3,20 @@ document.getElementById("contactstab").innerHTML = 'Add New';
 document.getElementById("opzioni").innerHTML = 'Options';
 document.getElementById("unitmisura").innerHTML = 'Unit of measure';
 document.getElementById("saveopt").value = 'Save';
+document.getElementById("sexleg").innerHTML = 'Sex:';
+document.getElementById("etaleg").innerHTML = 'Age (months):';
+document.getElementById("pesoleg").innerHTML = 'Weight:';
+document.getElementById("altleg").innerHTML = 'Height:';
+document.getElementById("calcoform").value = 'Calculate';
+
+document.getElementById("resetM").innerHTML = 'Clear all data';
+document.getElementById("resetF").innerHTML = 'Clear all data';
+document.getElementById("linktograph").innerHTML = 'Access the graphs';
+document.getElementById("h1male").innerHTML = 'Male';
+document.getElementById("h1female").innerHTML = 'Female';
+
+var ClearMessageM = 'All male datas have been cleared.';
+var ClearMessageF = 'All female datas have been cleared.';
+var completed = 'Completed';
+
+document.getElementById("istruzioni").innerHTML = '15 - 85 percentile : State of normality<br><span style="color: blue;">3 - 15 / 85 - 97 percentile : Mild pathology, to be monitored<span><br><span style="color: red;">&lt; 3 / &gt; 97 percentile : Blown pathology (visit recommended).</span><br>In relation to stature, a specialist examination is recommended (bone age evaluation, growth rate, hematochemical examinations, etc.)';
--- js/ita.js
+++ js/ita.js
@@ -3,3 +3,20 @@ document.getElementById("contactstab").innerHTML = 'Nuova Misura';
 document.getElementById("opzioni").innerHTML = 'Opzioni';
 document.getElementById("unitmisura").innerHTML = 'Unità di misura:';
 document.getElementById("saveopt").value = 'Salva';
+document.getElementById("sexleg").innerHTML = 'Sesso:';
+document.getElementById("etaleg").innerHTML = 'Età (mesi):';
+document.getElementById("pesoleg").innerHTML = 'Peso:';
+document.getElementById("altleg").innerHTML = 'Altezza:';
+document.getElementById("calcoform").value = 'Calcola';
+
+document.getElementById("resetM").innerHTML = 'Cancella tutti i dati';
+document.getElementById("resetF").innerHTML = 'Cancella tutti i dati';
+document.getElementById("linktograph").innerHTML = 'Vai ai grafici';
+document.getElementById("h1male").innerHTML = 'Maschio';
+document.getElementById("h1female").innerHTML = 'Femmina';
+
+var ClearMessageM = 'Tutti i dati relativi al profilo maschio sono stati cancellati.';
+var ClearMessageF = 'Tutti i dati relativi al profilo femmina sono stati cancellati.';
+var completed = 'Completato';
+
+document.getElementById("istruzioni").innerHTML = '15 - 85 percentile : Stato di normalità<br><span style="color: blue;">3 - 15 / 85 - 97 percentile : Patologia lieve e da monitorare</span><br><span style="color: red;">&lt; 3 / &gt; 97 percentile : Patologia conclamata (si consiglia visita).</span><br>In relazione alla statura si consiglia visita specialistica (valutazione dell’età ossea, della velocità di crescita, esami ematochimici, ecc)';
