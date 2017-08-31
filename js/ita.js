cec3fe8b9a8e9f1039328be814fcef9c49f00778
widonwaker <hey.lol@live.it>
2017-08-31 12:53:13 +0200

    Add files via upload

--- js/ita.js
+++ js/ita.js
@@ -1,24 +1,23 @@
-if (document.getElementById("tabB") !== null) { document.getElementById("tabB").innerHTML = 'Profili'; };
-if (document.getElementById("contactstab") !== null){ document.getElementById("contactstab").innerHTML = 'Nuova Misura';};
-if (document.getElementById("opzioni") !== null){ document.getElementById("opzioni").innerHTML = 'Opzioni';};
-if (document.getElementById("unitmisura") !== null){ document.getElementById("unitmisura").innerHTML = 'Unità di misura:';};
-if (document.getElementById("saveopt") !== null){ document.getElementById("saveopt").value = 'Salva';};
-if (document.getElementById("sexleg") !== null){ document.getElementById("sexleg").innerHTML = 'Sesso:';};
-if (document.getElementById("etaleg") !== null){ document.getElementById("etaleg").innerHTML = 'Età (mesi):';};
-if (document.getElementById("pesoleg") !== null){ document.getElementById("pesoleg").innerHTML = 'Peso:';};
-if (document.getElementById("altleg") !== null){ document.getElementById("altleg").innerHTML = 'Altezza:';};
-if (document.getElementById("calcoform") !== null){ document.getElementById("calcoform").value = 'Calcola';};
+if (document.getElementById("tabB") !== null) { document.getElementById("tabB").innerHTML = 'Profili'; }
+if (document.getElementById("contactstab") !== null){ document.getElementById("contactstab").innerHTML = 'Misura';}
+if (document.getElementById("opzioni") !== null){ document.getElementById("opzioni").innerHTML = 'Opzioni';}
+if (document.getElementById("unitmisura") !== null){ document.getElementById("unitmisura").innerHTML = 'Unità di misura:';}
+if (document.getElementById("saveopt") !== null){ document.getElementById("saveopt").value = 'Salva';}
+if (document.getElementById("sexleg") !== null){ document.getElementById("sexleg").innerHTML = 'Sesso:';}
+if (document.getElementById("etaleg") !== null){ document.getElementById("etaleg").innerHTML = 'Età (mesi):';}
+if (document.getElementById("pesoleg") !== null){ document.getElementById("pesoleg").innerHTML = 'Peso:';}
+if (document.getElementById("altleg") !== null){ document.getElementById("altleg").innerHTML = 'Altezza:';}
+if (document.getElementById("calcoform") !== null){ document.getElementById("calcoform").value = 'Calcola';}
 
-if (document.getElementById("resetM") !== null){ document.getElementById("resetM").innerHTML = 'Cancella tutti i dati';};
-if (document.getElementById("resetF") !== null){ document.getElementById("resetF").innerHTML = 'Cancella tutti i dati';};
-if (document.getElementById("linktograph") !== null){ document.getElementById("linktograph").innerHTML = 'Vai ai grafici';};
-if (document.getElementById("h1male") !== null) { document.getElementById("h1male").innerHTML = "Maschio"; };
-if (document.getElementById("h1female") !== null){ document.getElementById("h1female").innerHTML = "Femmina"; };
+if (document.getElementById("resetM") !== null){ document.getElementById("resetM").innerHTML = 'Cancella tutti i dati';}
+if (document.getElementById("resetF") !== null){ document.getElementById("resetF").innerHTML = 'Cancella tutti i dati';}
+if (document.getElementById("linktograph") !== null){ document.getElementById("linktograph").innerHTML = 'Vai ai grafici';}
+if (document.getElementById("linktograph") !== null){ document.getElementById("linktographF").innerHTML = 'Vai ai grafici';}
+ document.getElementById("h1male").innerHTML = "Maschio"; 
+if (document.getElementById("h1female") !== null){ document.getElementById("h1female").innerHTML = "Femmina"; }
 
-if (document.getElementById("istruzioni") !== null){ document.getElementById("istruzioni").innerHTML = '15 - 85 percentile : Stato di normalità<br><span style="color: blue;">3 - 15 / 85 - 97 percentile : Patologia lieve e da monitorare</span><br><span style="color: red;">&lt; 3 / &gt; 97 percentile : Patologia conclamata (si consiglia visita).</span><br>In relazione alla statura si consiglia visita specialistica (valutazione dell’età ossea, della velocità di crescita, esami ematochimici, ecc)';};
+if (document.getElementById("istruzioni") !== null){ document.getElementById("istruzioni").innerHTML = '15 - 85 percentile : Stato di normalità<br><span style="color: blue;">3 - 15 / 85 - 97 percentile : Patologia lieve e da monitorare</span><br><span style="color: red;">&lt; 3 / &gt; 97 percentile : Patologia conclamata (si consiglia visita).</span><br>In relazione alla statura si consiglia visita specialistica (valutazione dell’età ossea, della velocità di crescita, esami ematochimici, ecc)';}
 
 var ClearMessageM = 'Tutti i dati relativi al profilo maschio sono stati cancellati.';
 var ClearMessageF = 'Tutti i dati relativi al profilo femmina sono stati cancellati.';
 var completed = 'Completato';
-
-
